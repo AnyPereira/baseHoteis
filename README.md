ca166c8da778102d03888166a4cc7e8f72a89244
anypereira <any.tecnologia@gmail.com>
2019-12-15 23:58:57 -0300

    projeto final

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# baseHoteis
+# Back-end | {reprograma} + Mercado Livre | Dezembro/19 
+
+## Objetivo
+
+### Essa é uma API de base de hotéis, quem consumi-la terá o retorno de hoteis de varios estados brasileiros.
+### Nessa API será possível consultar por cidade e com opção de café da manhã.
+### Excluir, Incluir e atualizar hotéis.
+
+## Endpoints
+get(/hoteis)
+get(/cafeDaManha)
+get(/:nomeCidade)
+post(/hoteis)
+put(/:_id)
+delete(/:_id)
+
+## Suporte e Linguagens
+### NodeJS
+### MongoDB
+### APIDoc
+
--- public/apidoc/api_data.js
+++ public/apidoc/api_data.js
@@ -3,6 +3,7 @@ define({ "api": [
     "type": "get",
     "url": "/hoteis",
     "title": "",
+    "name": "baseHoteis",
     "group": "Hoteis__",
     "success": {
       "fields": {
@@ -11,8 +12,8 @@ define({ "api": [
             "group": "Success 200",
             "type": "Object[]",
             "optional": false,
-            "field": "hoteis",
-            "description": "<p>Base de Hoteis</p>"
+            "field": "Retorna",
+            "description": "<p>todos os hoteis cadastrados</p>"
           }
         ]
       },
@@ -26,7 +27,156 @@ define({ "api": [
     },
     "version": "0.0.0",
     "filename": "src/routes/hoteisRoute.js",
-    "groupTitle": "Hoteis__",
-    "name": "GetHoteis"
+    "groupTitle": "Hoteis__"
+  },
+  {
+    "type": "get",
+    "url": "/cafeDaManha",
+    "title": "",
+    "name": "baseHoteis",
+    "group": "Hoteis__",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "Retorna",
+            "description": "<p>apenas hoteis que possuem café da manhã</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Sucesso",
+          "content": "   HTTP/1.1 200 OK\n  [{\n     \"nome\": \"Ibis Berrini\",\n        \"cidade\": \"Sao Paulo\",\n        \"estado\": \"SP\",\n        \"pais\": \"Brasil\",\n        \"tipoQuarto\": \"Double\",\n        \"cafeManha\": true,\n        \"valorDiaria\": 150,\n        \"tipoFaturamento\": \"Direto\",\n        \"dataReserva\": [\n            {\n                \"dataInicial\": \"2020-01-01T02:00:00.000Z\",\n                \"dataFinal\": \"2020-12-01T02:00:00.000Z\"\n            }\n        ]\n}]",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/routes/hoteisRoute.js",
+    "groupTitle": "Hoteis__"
+  },
+  {
+    "type": "get",
+    "url": "/:nomeCidade",
+    "title": "",
+    "name": "baseHoteis",
+    "group": "Hoteis__",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "Retorna",
+            "description": "<p>apenas os hoteis a cidade informada</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Sucesso",
+          "content": "   HTTP/1.1 200 OK\n  [{\n     \"nome\": \"Ibis Berrini\",\n        \"cidade\": \"Sao Paulo\",\n        \"estado\": \"SP\",\n        \"pais\": \"Brasil\",\n        \"tipoQuarto\": \"Double\",\n        \"cafeManha\": true,\n        \"valorDiaria\": 150,\n        \"tipoFaturamento\": \"Direto\",\n        \"dataReserva\": [\n            {\n                \"dataInicial\": \"2020-01-01T02:00:00.000Z\",\n                \"dataFinal\": \"2020-12-01T02:00:00.000Z\"\n            }\n        ]\n}]",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/routes/hoteisRoute.js",
+    "groupTitle": "Hoteis__"
+  },
+  {
+    "type": "post",
+    "url": "/hoteis",
+    "title": "",
+    "name": "baseHoteis",
+    "group": "Hoteis__",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "Inseri",
+            "description": "<p>hoteis na base</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Sucesso",
+          "content": " HTTP/1.1 200 OK\n[{\n  \"status\": true,\n  \"mensagem\": \"Hotel incluído com sucesso!\"\n }]",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/routes/hoteisRoute.js",
+    "groupTitle": "Hoteis__"
+  },
+  {
+    "type": "put",
+    "url": "/:_id",
+    "title": "",
+    "name": "baseHoteis",
+    "group": "Hoteis__",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "Atualiza",
+            "description": "<p>os dados do hotel</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Sucesso",
+          "content": " HTTP/1.1 200 OK\n[{\n  \"message\": \"Dado(s) atualizado(s) com sucesso!\"\n }]",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/routes/hoteisRoute.js",
+    "groupTitle": "Hoteis__"
+  },
+  {
+    "type": "delete",
+    "url": "/:_id",
+    "title": "",
+    "name": "baseHoteis",
+    "group": "Hoteis__",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "Exclui",
+            "description": "<p>o hotel informado</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Sucesso",
+          "content": " HTTP/1.1 200 OK\n[{\n  \"message\": \"Hotel removido com sucesso!\"\n }]",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/routes/hoteisRoute.js",
+    "groupTitle": "Hoteis__"
   }
 ] });
